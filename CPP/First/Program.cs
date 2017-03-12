5052f412e179808f4c49451d49aad34258098727
Radoslaw Urbanczyk <radek.urbanczyk@gmail.com>
2017-03-12 20:25:30 +0100

    Initial Commit

---
+++ CPP/First/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace First
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hi!");
+		}
+	}
+}
